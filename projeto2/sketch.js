c16457245e1180685df9678513985b40be31fa5d
bibi11092006 <138732150+bibi11092006@users.noreply.github.com>
2023-08-30 22:24:59 -0300

    Add files via upload

---
+++ projeto2/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+createCanvas(600, 600);
+function draw("White") {
+  background(220);
+}
+stroke(blue)
+fill(red)
+
+  //console.log mouseIspressed
+
+if(mouse Is pressed)
+rect(mouseX, mouseY, 20, 35);
