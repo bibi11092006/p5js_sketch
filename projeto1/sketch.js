bf3b5865d043be772d47b4436f2c1c74feb94649
bibi11092006 <138732150+bibi11092006@users.noreply.github.com>
2023-08-30 22:24:25 -0300

    Add files via upload

---
+++ projeto1/sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background("white");
+  
+stroke(blue)  
+// console.log(mouselspressed);  
+  
+  
+rect(mouseX,mouseY,28,35);
+ifmouselspressed  
+  
+  
+}
